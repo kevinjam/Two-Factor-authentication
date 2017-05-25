52c0225ed39580ac91db1d562cf93211ebcfff41
kevinjam <kevin.janvier5@gmail.com>
2017-05-26 00:12:09 +0300

    authentication code

---
+++ app/src/main/java/com/nexmo/sdk/sample/verifysample/Config.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2015 Nexmo Inc
+ * All rights reserved.
+ *
+ * Licensed only under the Nexmo Verify SDK License Agreement located at
+ *
+ * https://www.nexmo.com/terms-use/verify-sdk/ (the “License”)
+ *
+ * You may not use, exercise any rights with respect to or exploit this SDK,
+ * or any modifications or derivative works thereof, except in accordance
+ * with the License.
+ */
+
+
+package com.nexmo.sdk.sample.verifysample;
+
+/**
+ * Encapsulate here your Nexmo applicationId and sharedSecretKey as provided by Nexmo per your application.
+ */
+public class Config {
+
+    public static final String NexmoAppId = "e6a0a053-9acb-4c35-9869-cab1efc1822c";
+    public static final String NexmoSharedSecretKey = "bea53ee16e09b1c";
+}
